b0430f8573613cba9ae74466e3248ab9d2534720
saramortara <saramortara@gmail.com>
2020-11-14 11:43:40 -0300

    updating code from Rmd

--- index.R
+++ index.R
@@ -1,11 +1,11 @@
-## ----vetores-------------------------------------------
+## ----vetores-----------------------------------------------------------------------------------------------------------------------
 populacao <- c(1500, 2000) 
 estados <- c("Acre", "Alagoas", "Amapá", "Amazonas", "Tocantins")
 class(populacao)
 class(estados)
 
 
-## ----vetores2------------------------------------------
+## ----vetores2----------------------------------------------------------------------------------------------------------------------
 verdadeiro_falso <- c(FALSE, TRUE)
 verdadeiro_falso
 class(verdadeiro_falso)
@@ -14,12 +14,12 @@ posicao
 class(posicao)
 
 
-## ----str_length----------------------------------------
+## ----str_length--------------------------------------------------------------------------------------------------------------------
 str(estados) #estrutura
 length(estados)
 
 
-## ----subsetting, results = "hide"----------------------
+## ----subsetting, results = "hide"--------------------------------------------------------------------------------------------------
 estados <- c("Acre", "Alagoas", "Amapá", "Amazonas", "Tocantins")
 estados[1]
 estados[c(TRUE, FALSE, TRUE, FALSE)]
@@ -29,7 +29,7 @@ estados[1:4]
 estados[c(1,3,5)] #tem que ser um vetor
 
 
-## ----logical_clauses, results="hide"-------------------
+## ----logical_clauses, results="hide"-----------------------------------------------------------------------------------------------
 casos <- c(150, 200, 400, 500, 500, 600)
 casos > 150
 casos[casos > 150]
@@ -39,11 +39,11 @@ casos == 200
 casos != 200
 
 
-## ------------------------------------------------------
+## ----------------------------------------------------------------------------------------------------------------------------------
 casos[casos > 150]
 
 
-## ----outras, results="hide"----------------------------
+## ----outras, results="hide"--------------------------------------------------------------------------------------------------------
 1:10
 seq(1, 10, 2)
 rep(1:4, 2)
@@ -51,7 +51,7 @@ rep(1:4, each = 2)
 unique(casos)
 
 
-## ----pacotes, eval = F---------------------------------
+## ----pacotes, eval = F-------------------------------------------------------------------------------------------------------------
 ## # Para instalar pacotes desde CRAN
 ## install.packages("remotes")
 ## 
@@ -65,21 +65,24 @@ unique(casos)
 ## ?coronabr
 
 
-## ----coronabr, echo = TRUE-----------------------------
+## ----coronabr, echo = TRUE---------------------------------------------------------------------------------------------------------
 library(coronabr)
 
 
-## ----get_corona_br, eval = FALSE-----------------------
+## ----get_corona_br, eval = FALSE---------------------------------------------------------------------------------------------------
 ## caminho <- "dados/brutos"
+## if (!dir.exists(caminho)) {
+##   dir.create(caminho)
+## }
 ## get_corona_br(dir = caminho, filename = "01-amapa", uf = "AP")
 
 
-## ----read_to_tranform_to_df----------------------------
+## ----read_to_tranform_to_df--------------------------------------------------------------------------------------------------------
 amapa <- read.csv("dados/brutos/01-amapa.csv", stringsAsFactors = FALSE)
 class(amapa)
 
 
-## ----try, echo = TRUE, eval = FALSE--------------------
+## ----try, echo = TRUE, eval = FALSE------------------------------------------------------------------------------------------------
 ## names(amapa)
 ## dim(amapa)
 ## nrow(amapa)
@@ -91,42 +94,50 @@ class(amapa)
 ## summary(amapa) # quantis e a média
 
 
-## ------------------------------------------------------
+## ----------------------------------------------------------------------------------------------------------------------------------
 amapa[, 1:3] #três primeiras colunas
 amapa[4, 13] #quarto elemento da coluna 13
 
 
 
-## ----echo = FALSE--------------------------------------
+## ----echo = FALSE------------------------------------------------------------------------------------------------------------------
 include_graphics("https://www.measureevaluation.org/resources/newsroom/news-images/tidy-data/image")
 
 
-## ---- echo = FALSE, out.width=300----------------------
+## ---- echo = FALSE, out.width=300--------------------------------------------------------------------------------------------------
 include_graphics("figs/data_feminism.png")
 
 
-## ------------------------------------------------------
+## ----------------------------------------------------------------------------------------------------------------------------------
 amapa <- read.csv("dados/brutos/01-amapa.csv")
 head(amapa[c(3, 4, 9:18)])
 
 
-## ------------------------------------------------------
+## ----------------------------------------------------------------------------------------------------------------------------------
 unique(amapa$city)
 
 
 
-## ------------------------------------------------------
+## ----------------------------------------------------------------------------------------------------------------------------------
 table(amapa$place_type)
 
 unique(amapa$place_type[is.na(amapa$city)])
 
 
-## ------------------------------------------------------
+## ----------------------------------------------------------------------------------------------------------------------------------
 class(amapa$date)
 class(amapa$last_available_date)
 
 
-## ------------------------------------------------------
+## ----------------------------------------------------------------------------------------------------------------------------------
+# Conversão para data
+data1 <- as.Date(amapa$date[1])
+data1
+# Mudando o formato com a função format()
+format(data1, "%d/%m/%y")
+format(data1, "%d/%m/%Y")
+
+# Mudando a data das colunas de data
 amapa$date <- as.Date(amapa$date)
 amapa$last_available_date <- as.Date(amapa$last_available_date)
 
@@ -135,43 +146,68 @@ class(amapa$date)
 range(amapa$date)
 
 
-## ------------------------------------------------------
+## ----------------------------------------------------------------------------------------------------------------------------------
 acu <- aggregate(last_available_confirmed ~ city, FUN = max, data = amapa)
 acu
 
 
 
-## ----fig.align='center'--------------------------------
+## ----fig.align='center'------------------------------------------------------------------------------------------------------------
 barplot(acu[, 2], names.arg = acu[, 1])
 
 
-## ------------------------------------------------------
-acu.or <- acu[order(acu$last_available_confirmed), ]
-acu.or
+## ----------------------------------------------------------------------------------------------------------------------------------
+acu_or <- acu[order(acu$last_available_confirmed), ]
+acu_or
 
 
-## ----fig.align='center'--------------------------------
-barplot(acu.or[, 2], names.arg = acu.or[, 1], las = 2)
+## ----fig.align='center', out.width=400---------------------------------------------------------------------------------------------
+barplot(acu_or[, 2], names.arg = acu_or[, 1], las = 2)
 
 
-## ------------------------------------------------------
+## ----fig.align='center', out.width=400---------------------------------------------------------------------------------------------
+# Lendo os dados
+muni_apagao <- read.csv("dados/brutos/municipios_apagao_11-2020.csv", 
+                        stringsAsFactors = FALSE)
+# Quantos municipios
+table(muni_apagao$apagao)
+# Juntando duas tabelas a partir de uma coluna comum
+acu_or_apagao <- merge(acu_or, muni_apagao, 
+                       by = "city", sort = FALSE)
+
+
+## ----fig.align='center', out.width=400---------------------------------------------------------------------------------------------
+head(acu_or)
+
+
+## ----fig.align='center', out.width=400---------------------------------------------------------------------------------------------
+head(acu_or_apagao)
+
+
+## ----fig.align='center', out.width=400---------------------------------------------------------------------------------------------
+barplot(acu_or_apagao[, 2], names.arg = acu_or_apagao[, 1], 
+        col = ifelse(acu_or_apagao$apagao == "sim", "grey", "orange"), las = 2)
+
+
+## ----------------------------------------------------------------------------------------------------------------------------------
 estado <- amapa[amapa$place_type == "state", c(-15)]
 dim(estado)
 
 
-## ------------------------------------------------------
+## ----------------------------------------------------------------------------------------------------------------------------------
 # outra forma: onde a coluna city é NA
 estado2 <- amapa[is.na(amapa$city), c(-15)]
 ## as duas formas tem o mesmo resultado:
 all.equal(estado2, estado)
 
 
-## ------------------------------------------------------
+## ----------------------------------------------------------------------------------------------------------------------------------
 # dados apenas de Macapa por meio do geocode do IBGE
 macapa <- amapa[amapa$city_ibge_code == "1600303", c(-15)]
+dim(macapa)
 
 
-## ------------------------------------------------------
+## ----------------------------------------------------------------------------------------------------------------------------------
 if (!dir.exists("dados/processados/")) {
   dir.create("dados/processados/")
 }
@@ -186,35 +222,35 @@ write.csv(macapa,
           row.names = FALSE)
 
 
-## ---- eval = FALSE-------------------------------------
+## ---- eval = FALSE-----------------------------------------------------------------------------------------------------------------
 ## plot(last_available_confirmed ~ date, data = macapa)
 ## lines(last_available_confirmed ~ date, data = macapa)
 
 
-## ---- echo = FALSE, fig.align='center'-----------------
+## ---- echo = FALSE, fig.align='center'---------------------------------------------------------------------------------------------
 plot(last_available_confirmed ~ date, data = macapa)
 lines(last_available_confirmed ~ date, data = macapa)
 
 
-## ---- fig.align='center', out.width=400, echo = FALSE----
+## ---- fig.align='center', out.width=400, echo = FALSE------------------------------------------------------------------------------
 include_graphics("./figs/AP_design_ativista.png")
 
 
-## ----eval = FALSE--------------------------------------
+## ----eval = FALSE------------------------------------------------------------------------------------------------------------------
 ## # para o estado todo
 ## plot(last_available_confirmed ~ date, data = estado,
 ##      xlab = "Data de notificação", ylab = "Casos acumulados")
 ## lines(last_available_confirmed ~ date, data = estado)
 
 
-## ----echo = FALSE, fig.align='center'------------------
+## ----echo = FALSE, fig.align='center'----------------------------------------------------------------------------------------------
 # para o estado todo
 plot(last_available_confirmed ~ date, data = estado,
      xlab = "Data de notificação", ylab = "Casos acumulados")
 lines(last_available_confirmed ~ date, data = estado)
 
 
-## ----eval = FALSE--------------------------------------
+## ----eval = FALSE------------------------------------------------------------------------------------------------------------------
 ## # para o estado todo
 ## plot(last_available_confirmed ~ date, data = estado,
 ##      xlab = "Data de notificação", ylab = "Casos acumulados")
@@ -228,7 +264,7 @@ lines(last_available_confirmed ~ date, data = estado)
 ## 
 
 
-## ----echo = FALSE, fig.align='center'------------------
+## ----echo = FALSE, fig.align='center'----------------------------------------------------------------------------------------------
 # para o estado todo
 plot(last_available_confirmed ~ date, data = estado,
      xlab = "Data de notificação", ylab = "Casos acumulados")
@@ -240,27 +276,27 @@ legend("topleft", c("estado AP", "município Macapá"),
 
 
 
-## ----eval = FALSE--------------------------------------
+## ----eval = FALSE------------------------------------------------------------------------------------------------------------------
 ## plot(x = amapa$estimated_population,
 ##      y = amapa$last_available_confirmed)
 
 
-## ----echo = FALSE, fig.align='center'------------------
+## ----echo = FALSE, fig.align='center'----------------------------------------------------------------------------------------------
 plot(x = amapa$estimated_population, 
      y = amapa$last_available_confirmed)
 
 
-## ----eval = FALSE--------------------------------------
+## ----eval = FALSE------------------------------------------------------------------------------------------------------------------
 ## boxplot(last_available_death_rate ~ city,
 ##         data = amapa)
 
 
-## ----echo = FALSE, fig.align='center'------------------
+## ----echo = FALSE, fig.align='center'----------------------------------------------------------------------------------------------
 boxplot(last_available_death_rate ~ city, 
         data = amapa)
 
 
-## ---- echo=FALSE, fig.show='asis', fig.align='center', fig.width=8, fig.height=7----
+## ---- echo=FALSE, fig.show='asis', fig.align='center', fig.width=8, fig.height=7---------------------------------------------------
 set.seed(2)
 par(bty = "n")
 bp <- rnorm(1000, 0, 0.1)
@@ -275,24 +311,24 @@ arrows(x0 = 0.68, x1 = 0.68, y0 = quantile(bp)[2],
 text(x = 0.54, y = median(bp), "IIQ", cex = .9)
 
 
-## ------------------------------------------------------
+## ----------------------------------------------------------------------------------------------------------------------------------
 # Macapa
 macapa <- read.csv("dados/processados/02-municipio_Macapa.csv")
 # convertendo para classe data
 macapa$date <- as.Date(macapa$date)
 
 
-## ----eval=FALSE----------------------------------------
+## ----eval=FALSE--------------------------------------------------------------------------------------------------------------------
 ## plot(new_confirmed ~ date, data = macapa)
 ## lines(new_confirmed ~ date, data = macapa)
 
 
-## ----echo=FALSE, fig.align='center'--------------------
+## ----echo=FALSE, fig.align='center'------------------------------------------------------------------------------------------------
 plot(new_confirmed ~ date, data = macapa)
 lines(new_confirmed ~ date, data = macapa)
 
 
-## ----eval = FALSE--------------------------------------
+## ----eval = FALSE------------------------------------------------------------------------------------------------------------------
 ## # usando a funcao par para controlar parametros
 ## par(bty = "l", las = 1)
 ## # adicionando nome dos eixos
@@ -302,7 +338,7 @@ lines(new_confirmed ~ date, data = macapa)
 ## lines(new_confirmed ~ date, data = macapa)
 
 
-## ----echo = FALSE, fig.align='center'------------------
+## ----echo = FALSE, fig.align='center'----------------------------------------------------------------------------------------------
 # usando a funcao par para controlar parametros
 par(bty = "l", las = 1)
 # adicionando nome dos eixos
@@ -312,26 +348,77 @@ plot(new_confirmed ~ date, data = macapa,
 lines(new_confirmed ~ date, data = macapa)
 
 
-## ----eval = FALSE--------------------------------------
+## ----eval = FALSE------------------------------------------------------------------------------------------------------------------
+## vermelho <- "#A70000" #ou rgb(167, 0, 0, maxColorValue = 255)
+## vermelho_trans <- rgb(167, 0, 0, alpha = 150, maxColorValue = 255)
+## 
+## # adicionando nome dos eixos
+## plot(new_confirmed ~ date, data = macapa,
+##      xlab = "Data",
+##      ylab = "Casos diários",
+##      col = vermelho)
+## lines(new_confirmed ~ date, data = macapa,
+##       col = vermelho_trans)
+
+
+## ----echo = FALSE, fig.align='center'----------------------------------------------------------------------------------------------
+vermelho <- "#A70000" #ou rgb(167, 0, 0, maxColorValue = 255)
+vermelho_trans <- rgb(167, 0, 0, alpha = 150, maxColorValue = 255)
+
+# adicionando nome dos eixos
+plot(new_confirmed ~ date, data = macapa,
+     xlab = "Data",
+     ylab = "Casos diários", 
+     col = vermelho) # muda cor
+lines(new_confirmed ~ date, data = macapa, 
+      col = vermelho_trans) # muda cor
+
+
+## ----eval = FALSE------------------------------------------------------------------------------------------------------------------
+## # adicionando nome dos eixos
+## plot(new_confirmed ~ date, data = macapa,
+##      xlab = "Data",
+##      ylab = "Casos diários",
+##      col = vermelho_trans, # muda cor
+##      pch = 19) # muda tipo de ponto
+## lines(new_confirmed ~ date, data = macapa,
+##       col = vermelho_trans)
+
+
+## ----echo = FALSE, fig.align='center'----------------------------------------------------------------------------------------------
+# adicionando nome dos eixos
+plot(new_confirmed ~ date, data = macapa,
+     xlab = "Data",
+     ylab = "Casos diários", 
+     col = vermelho_trans, 
+     pch = 19) # muda tipo de ponto
+lines(new_confirmed ~ date, data = macapa, 
+      col = vermelho_trans)
+
+
+## ----eval = FALSE------------------------------------------------------------------------------------------------------------------
 ## png("figs/casos_diarios_Macapa.png", res = 300,
 ##     width = 1200, height = 1200)
 ## par(bty = "l", las = 1)
 ## # adicionando nome dos eixos
 ## plot(new_confirmed ~ date, data = macapa,
 ##      xlab = "Data",
-##      ylab = "Casos diários")
-## lines(new_confirmed ~ date, data = macapa)
+##      ylab = "Casos diários",
+##      col = vermelho_trans, # muda cor
+##      pch = 19) # muda tipo de ponto
+## lines(new_confirmed ~ date, data = macapa,
+##       col = vermelho_trans)
 ## dev.off()
 
 
-## ----gif, echo = FALSE, out.width=500, fig.align='center'----
+## ----gif, echo = FALSE, out.width=500, fig.align='center'--------------------------------------------------------------------------
 include_graphics("https://raw.githubusercontent.com/covid19br/covid19br.github.io/master/fig/gif_BE_05_junho.gif")
 
 
-## ---- echo = FALSE, fig.align='center'-----------------
+## ---- echo = FALSE, fig.align='center'---------------------------------------------------------------------------------------------
 include_graphics("./figs/observatorio.png")
 
 
-## ----obs-diarios,echo = FALSE, fig.align='center', out.width=700----
+## ----obs-diarios,echo = FALSE, fig.align='center', out.width=700-------------------------------------------------------------------
 include_graphics("./figs/diarios_AP.png")
 
